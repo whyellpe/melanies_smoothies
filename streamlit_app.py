d9065a896804dd90cca67529ccef5f500f8c9050
ylp <64739014+whyellpe@users.noreply.github.com>
2025-09-26 13:13:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe,max_selections=5)
 
+# Convert snowpark dataframe to pandas dataframe so we can use LOC function
 pd_df=my_dataframe.to_pandas()
 
 if ingredients_list:
@@ -33,20 +34,14 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-        # st.stop()
-
-    # Convert snowpark dataframe to pandas dataframe so we can use LOC function
-    
-    # st.dataframe(pd_df)
-    # st.stop()
     
     st.write (ingredients_string)
     
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
-    st.write(my_insert_stmt)
-    # st.stop()
+    # st.write(my_insert_stmt)
+
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
