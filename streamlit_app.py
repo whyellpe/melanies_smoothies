a5121aa33a7b342958821617720b80c4e65cf7ca
ylp <64739014+whyellpe@users.noreply.github.com>
2025-09-26 13:11:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ if ingredients_list:
 
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-        # st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+        st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         # st.stop()
 
     # Convert snowpark dataframe to pandas dataframe so we can use LOC function
